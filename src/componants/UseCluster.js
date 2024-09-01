f0f75dd45cd4ce52efebe98d548ad55b43d6839c
coderFeedForwardAlg <gaspersscottma@gmail.com>
2024-08-31 21:16:03 -0400

    upload

--- src/componants/UseCluster.js
+++ src/componants/UseCluster.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 
 function UseCluter() {
+    const [responseData, setResponseData] = useState('');
     const [file, setFile] = useState()
 
   function handleChange(event) {
@@ -12,30 +13,40 @@ function UseCluter() {
   
   async function handleSubmit(event) {
     event.preventDefault();
-    const url = 'http://127.0.0.1:8000';
+    const url = 'http://38.70.48.137:8000/run_ray';   //'http://127.0.0.1:8000';
     const formData = new FormData();
     formData.append('file', file);
     // formData.append('fileName', file.name);
     
-    let response = await fetch('http://127.0.0.1:8000', {
+    let response = await fetch(url, {
         method: 'POST',
         headers: {
+
           'Content-Type': 'multipart/form-data',
         },
         body: formData,
-      }).then((response) => {
-        console.log(response)
-      });
+      })
+
+      const result = await response.text();
+      if (result) {
+        setResponseData(result);
+      }
 
   }
 
+  
+
+
   return (
     <div className="hstack">
+        
        <form onSubmit={handleSubmit}>
-          <h1>React File Upload</h1>
+          <h1 style={{color: "white"}}>React File Upload</h1>
           <input type="file" onChange={handleChange}/>
           <button type="submit">Upload</button>
         </form>
+        
+        {responseData && <h2 style={{color: "white"}}>{responseData}</h2>}
     </div>
   );
 }
